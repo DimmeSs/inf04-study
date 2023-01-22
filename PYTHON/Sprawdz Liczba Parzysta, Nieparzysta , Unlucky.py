e0e49b856f7fc07c43ebf78cefb2ddc4c19f0d56
DimmeSs <karol2016pl@gmail.com>
2023-01-22 22:34:48 +0100

    Dodatkowe materiały

--- PYTHON/Sprawdz Liczba Parzysta, Nieparzysta , Unlucky.py
+++
@@ -1,8 +0,0 @@
-numb = 0
-for ilosc in range(1,21):
-    numb +=1
-    if numb == 3 or numb == 13: answ = "UNLUCKY"
-    elif numb % 2 == 0:answ = "even"
-    else: answ = "odd"
-    print(numb,"is",answ)
-input("Wciśnij enter aby zakończyć")
